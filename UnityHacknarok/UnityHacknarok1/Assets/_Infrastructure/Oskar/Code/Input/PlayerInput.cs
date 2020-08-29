f56c5a47ab84e059c49f6c013f913deb2bdd5866
Oskar Kogut <kogutoskar123@gmail.com>
2020-08-29 14:48:13 +0200

    Add interaction key to input system

--- UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Input/PlayerInput.cs
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Input/PlayerInput.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,9 +7,13 @@ namespace Infrastructure
 {
     public class PlayerInput : MonoBehaviour
     {
+        [SerializeField] private string _interactionKey;
+        
         private const string HORIZONTAL_AXIS_KEY = "Horizontal";
         private const string VARTICAL_AXIS_KEY = "Vertical";
 
+        public Action InteractionKeyPressed;
+        
         public float HorizontalAxis { get; private set; }
 
         public float VerticalAxis { get; private set; }
@@ -18,6 +23,11 @@ namespace Infrastructure
         {
             HorizontalAxis = Input.GetAxis(HORIZONTAL_AXIS_KEY);
             VerticalAxis = Input.GetAxis(VARTICAL_AXIS_KEY);
+
+            if (Input.GetButtonDown(_interactionKey))
+            {
+                print("chuj");
+            }
         }
     }
 }
