68712f3796d62c426cd0f68e76d9506722d1b76e
Oskar Kogut <kogutoskar123@gmail.com>
2020-08-29 12:47:21 +0200

    Add abstract layer over player input

---
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Input/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        private const string HORIZONTAL_AXIS_KEY = "Horizontal";
+        private const string VARTICAL_AXIS_KEY = "Vertical";
+
+        public float HorizontalAxis { get; private set; }
+
+        public float VerticalAxis { get; private set; }
+
+
+        private void Update()
+        {
+            HorizontalAxis = Input.GetAxis(HORIZONTAL_AXIS_KEY);
+            VerticalAxis = Input.GetAxis(VARTICAL_AXIS_KEY);
+        }
+    }
+}
