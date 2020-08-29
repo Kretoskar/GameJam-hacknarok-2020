e33e6701d444295994b4325474a06c566e8c55d7
Oskar Kogut <kogutoskar123@gmail.com>
2020-08-29 13:25:26 +0200

    Fix rb move stop hardness

--- UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Player/Mover.cs
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Player/Mover.cs
@@ -8,6 +8,7 @@ namespace Infrastructure.Player
     public class Mover : MonoBehaviour
     {
         [SerializeField] [Range(0.1f, 100)] private float _speed;
+        [SerializeField] [Range(0.1f, 1)] private float _stopHardness = 0.1f;
         
         private Rigidbody2D _rb;
 
@@ -31,6 +32,9 @@ namespace Infrastructure.Player
 
         private void MoveRigidbody()
         {
+            if(_moveVector.magnitude <= _stopHardness)
+                _rb.velocity = Vector2.zero;
+            
             _rb.MovePosition(_rb.position + _moveVector * _speed * Time.fixedDeltaTime);
         }
     }
