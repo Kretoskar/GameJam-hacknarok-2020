927de2c83dd46718af78c27a01d3d95da19e2588
Oskar Kogut <kogutoskar123@gmail.com>
2020-08-29 21:30:25 +0200

    Add enemy chase stsate

---
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Enemy/EnemyStateMachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Infrastructure.Player;
+using UnityEngine;
+
+namespace Infrastructure.Enemy
+{
+    public class EnemyStateMachine : MonoBehaviour
+    {
+        [SerializeField] [Range(0,1000)] private float _speed = 5;
+        [SerializeField] private float _chaseDistance = 5;
+        [SerializeField] private float _attackDistance = 5;
+
+        private Rigidbody2D _rb;
+        private Transform _player;
+
+        void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Start()
+        {
+            _player = FindObjectOfType<PlayerControler>().transform;
+        }
+
+        void Update()
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, _player.position);
+
+            if (distanceToPlayer < _attackDistance)
+            {
+                Attack();
+                return;
+            }
+
+            if(distanceToPlayer < _chaseDistance)
+                Chase();
+        }
+
+        void Attack()
+        {
+            _rb.velocity = Vector2.zero;
+        }
+        
+        void Chase()
+        {
+            _rb.velocity = (_player.position - transform.position).normalized * _speed * Time.deltaTime;
+        }
+    }
+}
