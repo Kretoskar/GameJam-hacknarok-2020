c9f72b7b1643f0aa758c8d118dff50b394b3f1eb
Piotr Ksiazek <piotr.ksiazek.mw@gmail.com>
2020-08-30 09:49:00 +0200

    Background Music

---
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Piotr/Muzyka miedzyscenie/BgMusic.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgMusic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Play Global
+    private static BgMusic instance = null;
+    public static BgMusic Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    //Play Gobal End
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
