c8da4e946229e5cdaa35d58c0c06426976778bea
Oskar Kogut <kogutoskar123@gmail.com>
2020-08-30 09:44:06 +0200

    Add RequireItemAddPoints

---
+++ UnityHacknarok/UnityHacknarok1/Assets/_Infrastructure/Oskar/Code/Interactions/RequireItemAddPoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Infrastructure;
+using Infrastructure.Interactions;
+using Infrastructure.LevelManagement;
+using UnityEngine;
+
+public class RequireItemAddPoints : InteractionRequireItem
+{
+    [SerializeField] private int _addPoints;
+    [SerializeField] private int _addTime;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        if (_hasItem)
+        {
+            Timer.Instance.AddTime(_addTime);
+            ScoreManager.Instance.AddScore(_addPoints);
+        }
+    }
+}
